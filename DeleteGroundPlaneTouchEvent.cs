15fcc180d0e2ffd14cec3e1300515086cdef5637
VC00746 <aterada@M-148noMac-mini.local>
2019-05-14 20:47:41 +0900

    add scripts to delete GroundPLane touch event

---
+++ DeleteGroundPlaneTouchEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DeleteGroundPlaneTouchEvent : MonoBehaviour
+{
+    PlaneFinderBehaviour planeFinderBehaviour = null;
+
+    public void RemovePlaneTouchEvent()
+    {
+        planeFinderBehaviour.OnInteractiveHitTest = null;
+    }
+}
